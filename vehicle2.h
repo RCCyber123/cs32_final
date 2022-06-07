d3a1ec424dc0775b1da9401075e109ce6b81ecb0
Ryan Chau <ryanchau@csilvm-01.cs.ucsb.edu>
2022-06-07 13:17:07 -0700

    done

--- vehicle2.h
+++ vehicle2.h
@@ -1,3 +1,6 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
 #include <string>
 using namespace std;
 
@@ -53,26 +56,4 @@ private:
    int year; 
 };
 
-class Car : public Vehicle { 
-   public:
-        Car() : Vehicle(){
-          ED = 0;
-          HP = 0;
-        }
-
-        Car(string vehicle_class, string origin, int year, string color, double ED, int HP) : Vehicle(vehicle_class, origin, year, color){
-          this->ED = ED;
-          this->HP = HP;
-        }
-
-        double get_ED() const{ return ED; }
-        int get_HP() const{ return HP; }
-
-	   void set_ED(double ED){ this->ED = ED; } 
-        void set_HP(int HP){ this->HP = HP; }
-
-
-   private: 
-	double ED; 
-     int HP;
-}; 
+#endif
